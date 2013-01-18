ccf33d0e2673398b3ea3da65e0e8942ec60d7d48
Josh Smith <theoperant@gmail.com>
2013-01-17 21:08:20 -0600

    Added some more comments as well as removing a bit of old code. Initial todos are still in effect.

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -3,10 +3,13 @@
 #include <Servo.h>
 #include <LiquidCrystal.h>
 
+//These are the various pins connected to the victors and the pots
 int VictorPin1 = 9;
 int VictorPin2 = 10;
 int VictorPot1 = A0;
 int VictorPot2 = A1;
+
+//Leave these variables alone. These are set every time through loop
 int PotValue1 = 0;
 int PotValue2 = 0;
 int Rpm1 = 0;
@@ -17,16 +20,20 @@ int MaxRpm = 7000;
 //Put up to 16 characters here of the name of the motor you are testing.
 char MotorName[16] = "CIM MOTOR";
 
+//Set the pins to be used for the LCD display
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup()
 {
+  //Set our pins to output to send the pwm signal to the victors
   pinMode(VictorPin1, OUTPUT);
   pinMode(VictorPin2, OUTPUT);
   
+  //Initialize the LCD
   lcd.begin(16, 2);
   lcd.clear();
   
+  //Starting up screens
   lcd.print("Wildstang Motor");
   lcd.setCursor(0, 1);
   lcd.print("Controller");
@@ -42,6 +49,8 @@ void setup()
   delay(4000);
   lcd.clear();
   
+  //We start with a "zeroed out" display to make things a bit easier.
+  //The RPM section is simply rewritten as needed while leaving the static bits.
   lcd.setCursor(0, 0);
   lcd.print("Victor1: 0000RPM");
   lcd.setCursor(0, 1);
@@ -50,39 +59,20 @@ void setup()
 
 void loop()
 {
+  //Grab the values from the pots
   PotValue1 = analogRead(VictorPot1);
   PotValue2 = analogRead(VictorPot2);
   
+  //Map these values to a new range of 0-255. This is what the victors expect.
   PotValue1 = map(PotValue1, 0, 1023, 0, 255);
   PotValue2 = map(PotValue2, 0, 1023, 0, 255);
   
+  //For displaying the values, we want the max rpm to be shown instead of our 255 range.
+  //This will handle both positive and negative values (forward and reverse)
   Rpm1 = map(PotValue1, 0, 255, (-1*MaxRpm), MaxRpm);
   Rpm2 = map(PotValue2, 0, 255, (-1*MaxRpm), MaxRpm);
   
-  /*
-  if(Rpm1 < 0)
-  {
-    lcd.setCursor(8, 0);
-    lcd.print("-");
-  }
-  else
-  {
-    lcd.setCursor(8, 0);
-    lcd.print(" ");
-  }
-  
-  if(Rpm2 < 0)
-  {
-    lcd.setCursor(8, 1);
-    lcd.print("-");
-  }
-  else
-  {
-    lcd.setCursor(8, 1);
-    lcd.print(" ");
-  }
-  */
-  
+  //We set the first victor's rpm display here
   if(Rpm1 < 0)
   {
     if(Rpm1 < -999)
@@ -93,7 +83,11 @@ void loop()
     else if(Rpm1 < -99)
     {
       lcd.setCursor(8, 0);
+      
+      //This line covers up the previous bits from other values.
+      //It is a bit easier than clearing the whole display and rewriting the whole thing.
       lcd.print(" ");
+      
       lcd.setCursor(9, 0);
       lcd.print(Rpm1);
     }
@@ -142,6 +136,7 @@ void loop()
     }
   }
   
+  //We set the second victor's rpm display here
   if(Rpm2 < 0)
   {
     if(Rpm2 < -999)
@@ -201,6 +196,7 @@ void loop()
     }
   }
   
+  //Finally, we write these values to the victors. This bit still needs to be tested!
   analogWrite(VictorPin1, PotValue1);
   analogWrite(VictorPin2, PotValue2);
 }
